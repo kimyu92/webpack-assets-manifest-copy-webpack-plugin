06ab7e25463460b3c021e049ac0dd8947e29825c
Kim Yu Ng <kimyu92@outlook.com>
2022-07-10 20:28:49 -0500

    Merge manifest entry for assets loaded in assets module and copy-webpack-plugin

--- README.md
+++ README.md
@@ -12,3 +12,23 @@ A sample project to produce `manifest.json` for static assets that can be looked
 npm install
 npm run dev
 ```
+
+## Output
+Note: even though we export `svg/star.svg` via `copy-webpack-plugin` and [assets-modules](https://webpack.js.org/guides/asset-modules/), it's being merged under the same entry
+```json
+{
+  "entrypoints": {
+    "main": {
+      "assets": {
+        "js": [
+          "/assets/main.js"
+        ]
+      }
+    }
+  },
+  "fonts/Miglia.ttf": "/assets/fonts/Miglia-767f77adc54aa614023b.ttf",
+  "main.js": "/assets/main.js",
+  "svg/circle.svg": "/assets/svg/circle-cabec827907f07a5cf26.svg",
+  "svg/star.svg": "/assets/svg/star-50a73fe17181f247e4c3.svg"
+}
+```
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,46 @@ const path = require('path');
 
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 const CopyPlugin = require('copy-webpack-plugin');
+const { join } = require('path');
 
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
+    clean: true,
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/assets/',
-    assetModuleFilename: '[name][ext]',
+    assetModuleFilename: '[name]-[contenthash][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: join('svg', '[name]-[contenthash][ext]'),
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss', '.css'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss', '.css', '.svg'],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: path.join(__dirname, 'assets', 'fonts'), to: 'fonts' },
-        { from: path.join(__dirname, 'assets', 'svg'), to: 'svg' },
+        { from: path.join(__dirname, 'assets', 'fonts'), to: 'fonts/[name]-[contenthash][ext]' },
+        { from: path.join(__dirname, 'assets', 'svg'), to: 'svg/[name]-[contenthash][ext]' },
       ],
     }),
     new WebpackAssetsManifest({
